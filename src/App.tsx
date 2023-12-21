c59426225ad74976e169e8a24870b071d55ec910
cbailey17 <a.cameronbailey@gmail.com>
2023-12-21 07:34:54 -0800

    adding slight changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Canvas } from '@react-three/fiber'
-import { Experience } from './components/Experience'
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from '@react-three/fiber';
+import { Experience } from './components/Experience';
 
 function App() {
- 
-
   return (
-    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+    <Canvas shadows camera={{ position: [0, 3, 5], fov: 30 }}>
       <color attach="background" args={['#ececec']} />
       <Experience />
+      <axesHelper args={[5]} />
     </Canvas>
-  )
+  );
 }
 
-export default App
+export default App;
